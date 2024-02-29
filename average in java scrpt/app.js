83bea0a51259c833f50f92a652528d525e1eb72a
Muhammad Ahmed Siddiqui <154745545+mahmedsiddiqui@users.noreply.github.com>
2024-02-29 11:45:58 +0500

    Add files via upload

---
+++ average in java scrpt/app.js
@@ -0,0 +1,7 @@
+const num1 = parseInt(prompt('Enter Your First Numberto Add'));
+const num2 = parseInt(prompt('Enter Your Second Number Add '));
+const num3 = parseInt(prompt('Enter Your Second Number Add '));
+const num4 = parseInt(prompt('Enter Your Second Number Add '));
+const sum = num1+ num2 + num3 + num4 ;
+const average= sum/4
+document.write(" your average is ",    average)   
